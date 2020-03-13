b83a32adf24d17ab234652ffc3d0a646a14f1aef
Hamilton Chevez <hchev001@gmail.com>
2020-03-12 20:18:16 -0400

    fixed naming convention of sign in route

--- src/routes/user-routes/auth.-routes.js
+++ src/routes/user-routes/auth.-routes.js
@@ -1,18 +1,12 @@
 import AuthController from "../../controllers/user.controller/auth.controller";
 
 export default app => {
+  app.route("/user/sign_in").post(AuthController.sign_in);
 
-  app.route("user/sign_in")
-    .post(AuthController.sign_in)
-  
-  app.route("/user/sign_up")
-    .post(AuthController.sign_up);
-  
-  app.route("/auth/create_admin")
-    .post(AuthController.create_admin);
-  
-  app.route("/user/change_password")
-    .post(AuthController.change_password);
-  app.route("/user/_forget_password")
-    .post(AuthController.forget_password)
-}
+  app.route("/user/sign_up").post(AuthController.sign_up);
+
+  app.route("/auth/create_admin").post(AuthController.create_admin);
+
+  app.route("/user/change_password").post(AuthController.change_password);
+  app.route("/user/_forget_password").post(AuthController.forget_password);
+};
