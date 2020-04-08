2afe07cfd8ab72e649b2b3833726383483f169a8
Hamilton Chevez <hchev001@gmail.com>
2020-04-08 14:26:18 -0400

    how to check if user is authenticated

--- src/routes/user-routes/user-routes.js
+++ src/routes/user-routes/user-routes.js
@@ -1,21 +1,18 @@
 import UserController from "../../controllers/user.controller/user.controller";
 import AuthController from "../../controllers/user.controller/auth.controller";
-export default app =>{
-   
+import isAuthenticated from "../../services/middleware/auth.middleware";
+export default (app) => {
+  app.route("/signup").post(AuthController.sign_up);
 
-   app.route('/signup')
-    .post(
-      AuthController.sign_up
-    )
+  app.route("/login").post(AuthController.sign_in);
 
-   app.route("/login")
-    .post(AuthController.sign_in)
-
-    app.route("/users")
-        .get(UserController.list_all_users)
-        .post(UserController.create_a_user);
-    app.route("/users/:id")
-        .get(UserController.read_a_user)
-        .put(UserController.update_a_user)
-        .delete(UserController.delete_a_user)
-}
+  app
+    .route("/users")
+    .get(isAuthenticated(), UserController.list_all_users)
+    .post(UserController.create_a_user);
+  app
+    .route("/users/:id")
+    .get(UserController.read_a_user)
+    .put(UserController.update_a_user)
+    .delete(UserController.delete_a_user);
+};
